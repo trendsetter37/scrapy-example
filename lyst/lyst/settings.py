0315c593337c62214911bc01fe31e4014b280b26
Javis Sullivan <javissullivan@gmail.com>
2015-10-12 12:46:48 -0400

    Add Referer URL

--- lyst/lyst/settings.py
+++ lyst/lyst/settings.py
@@ -41,10 +41,11 @@ COOKIES_ENABLED=False
 #TELNETCONSOLE_ENABLED=False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+   'Referer': 'http://careers.stackoverflow.com/jobs/94455/web-scraping-engineer-remote-8-week-contract-lyst'
+}
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
