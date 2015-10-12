7bfa14bcf3eaa2034fb0b19f90bf4887964928c2
Javis Sullivan <javissullivan@gmail.com>
2015-10-12 13:29:48 -0400

    made alterations to initial requests

--- lyst/lyst/spiders/lyst_spider.py
+++ lyst/lyst/spiders/lyst_spider.py
@@ -1,7 +1,6 @@
-from scrapy import Spider
+from scrapy import Spider, Request
 from scrapy.selector import Selector
 from lyst.items import LystItem
-import urlparse
 
 '''
 product details xpath = '//div[@class="product-card"]/div[@class="product-details"]'
@@ -11,6 +10,19 @@ class LystSpider(Spider):
     name = 'lyst'
     allowed_domains = ['http://www.lyst.com/']
     start_urls = ['http://www.lyst.com/new-today/']
+    cookies = {
+        '__ssid':'eacf9496-15a3-40b4-b67c-d482f7d947ed',
+        '_ga' : 'GA1.2.1819590536.1444660498',
+        'analytics1': '4d4c96fa-8981-4b44-b45b-4a3ba14fcbbd:1:1:1444669394:16443ce3-1fb5-4fac-bcb9-7524f7dd0db1',
+        'country': 'US',
+        'sessionid':'ylvmjqpb5kwbpuqa4uwqhk5bihu7wgf5',
+        'withfonts': '1'
+    }
+
+    def start_requests(self):
+        for url in start_urls:
+            request = Request(url, cookies=cookies, callback=self.parse)
+            yield request
 
     def parse(self, response):
         products = Selector(response).xpath('//div[@class="product-card"]')
